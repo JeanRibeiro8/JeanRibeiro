c1268791218a4e18a4d3f120515e02607337d624
Jean <jeanrsantos10@gmail.com>
2026-08-16 16:42:32 -0300

    eng-red-update

--- src/script/script.js
+++ src/script/script.js
@@ -10,3 +10,29 @@
                 }
             });
         });
+
+
+//FILTER
+
+    const filterSelect = document.getElementById('projectFilter');
+    const projectCards = document.querySelectorAll('.project-card');
+
+    filterSelect.addEventListener('change', function () {
+        const selected = this.value;
+
+        projectCards.forEach(card => {
+            const category = card.getAttribute('data-category');
+
+            if (selected === 'all' || category === selected) {
+                card.style.display = 'block';
+            
+                card.style.opacity = '0';
+                setTimeout(() => {
+                    card.style.opacity = '1';
+                    card.style.transition = 'opacity 0.3s ease';
+                }, 50);
+            } else {
+                card.style.display = 'none';
+            }
+        });
+    });
